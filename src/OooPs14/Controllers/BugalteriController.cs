c0d9cf2a6189233d1a02c3cb4ba61dd22c5e4d06
Katy <Katy@MSQL>
2017-02-13 15:28:30 +0200

    Дополнение

---
+++ src/OooPs14/Controllers/BugalteriController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OooPs14.Controllers
+{
+    public class BugalteriController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/OooPs14/Models/Bugalteriya/BugalteriViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OooPs14.Models.Bugalteriya
+{
+    public class BugalteriViewModel
+    {
+        public string Name { get; set; }
+    }
+}
